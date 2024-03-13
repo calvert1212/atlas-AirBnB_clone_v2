8da058cddf4c69ed8aee321aaf3eb4da3d3278d2
calvert1212 <calvert1212@gmail.com>
2024-03-12 19:59:58 -0500

    Update 2-c_route.py
    Replaced more ' ' with " "

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,8 +18,8 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+    return "C {}".format(escape(text.replace("_", " ")))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
