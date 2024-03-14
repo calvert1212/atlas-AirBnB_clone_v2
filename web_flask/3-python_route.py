2f413133a83bd69abbe0dd25d5c5905f1bb26e6e
calvert1212 <calvert1212@gmail.com>
2024-03-13 19:04:58 -0500

    Update 3-python_route.py
    Fixed extra ) error

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    return 'Python ' + text.replace("_", " "))
+    return 'Python ' + text.replace("_", " ")
 
 
 if __name__ == '__main__':
